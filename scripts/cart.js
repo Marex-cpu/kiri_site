b13fcfd63a1383521590ca0323b533dc8b3f143d
marko-petrovic <markoo3871@gmail.com>
2023-02-15 11:56:04 +0100

    shop finish logic and style, as well cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,6 +2,8 @@
 const cartEmptyLabel = document.getElementById("cart-empty-label");
 const shopingCart = document.getElementById("shoping-cart");
 const shopTable = document.getElementById("shop-table");
+const totalPrice = document.getElementById("cart-totals");
+const totalSection = document.getElementById("total-section");
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
@@ -24,9 +26,53 @@ calculation();
 
 let generateCartItems = () => {
   if (basket.length !== 0) {
-    // shopingCart.innerHTML = basket.map((item) => {
-    //   return ``;
-    // });
+    shopingCart.innerHTML = basket
+      .map((obj) => {
+        let { id, item } = obj;
+        let search = shopItemsData.find((item) => item.id === id) || [];
+        return `<tr class="product">
+                  <td class="product-remove">
+                    <img
+                      width="25"
+                      src="/assets/indexPage/close-icon.svg"
+                      alt="close-icon"
+                      onclick="removeItem(${id})"
+                    />
+                  </td>
+                  <td class="product-thumbnail">
+                    <img src=${search.imageSrc} alt=${search.alt} />
+                  </td>
+                  <td class="product-name product-moblie" data-title="Proizvod">
+                    ${search.alt}
+                  </td>
+                  <td class="product-price product-moblie" data-title="Cena">
+                    ${search.price}.0 din
+                  </td>
+                  <td class="product-quantity product-moblie" data-title="Količina">
+                    <div class="buttons">
+                      <div onclick="decrement(${id})" id="decrement" class="decrement">
+                        <img
+                          width="20"
+                          src="/assets/indexPage/minus-icon.svg"
+                          alt="minus-icon"
+                        />
+                      </div>
+                      <div id="${id}" class="quantity">${item}</div>
+                      <div onclick="increment(${id})" id="increment" class="increment">
+                        <img
+                          width="20"
+                          src="/assets/indexPage/plus-icon.svg"
+                          alt="plus-icon"
+                        />
+                      </div>
+                    </div>
+                  </td>
+                  <td class="product-subtotal product-moblie" data-title="Ukupno">
+                    ${item * search.price}.0 din
+                  </td>
+                </tr>`;
+      })
+      .join("");
 
     cartEmptyLabel.style.display = "none";
   } else {
@@ -35,3 +81,75 @@ let generateCartItems = () => {
   }
 };
 generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  generateCartItems();
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+
+  update(selectedItem.id);
+
+  basket = basket.filter((x) => x.item !== 0);
+  generateCartItems();
+  totalAmount();
+
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+  totalAmount();
+};
+
+let removeItem = (id) => {
+  let selectedItem = id;
+
+  basket = basket.filter((obj) => obj.id !== selectedItem.id);
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+
+  alert("PROIZVOD IZBRISAN IZ KORPE!", location.reload());
+};
+
+let totalAmount = () => {
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((obj) => {
+        let { item, id } = obj;
+        let search = shopItemsData.find((item) => item.id === id) || [];
+        return item * search.price;
+      })
+      .reduce((prevPrice, nextPrice) => prevPrice + nextPrice, 0);
+    totalSection.style.display = "flex";
+    totalPrice.innerHTML = `<span class='cart-subtotal'>Ukupno:</span>
+                            <span class='price-total'>${amount}.00 din</span>`;
+  } else {
+    totalSection.style.display = "none";
+  }
+};
+totalAmount();
--- scripts/shopItemsData.js
+++ scripts/shopItemsData.js
@@ -2,21 +2,21 @@ let shopItemsData = [
   {
     id: "kdafnidas",
     imageSrc: "/assets/indexPage/dessert-img.webp",
-    alt: "desert od šljive",
+    alt: "Desert od šljive",
     detailTitle: "Desert od šljive 90 grama, izvor biotina I vlakana ",
     price: 139.0,
   },
   {
     id: "tdgfnidas",
     imageSrc: "/assets/indexPage/orasnice.webp",
-    alt: "proizvod orasnica",
+    alt: "Orasnica",
     detailTitle: "Orasnica 50 grama, izvor vitamina I minerala",
     price: 89.0,
   },
   {
     id: "gdafsidas",
     imageSrc: "/assets/indexPage/med_i_suncokret.webp",
-    alt: "proizvod plocica od semenki suncokreta i livadskog meda",
+    alt: "Pločica od semenki suncokreta i livadskog meda",
     detailTitle:
       "Pločica od semenki suncokreta i livadskog meda 50 grama, proizvod sa 10 benefita",
     price: 89.0,
@@ -24,28 +24,28 @@ let shopItemsData = [
   {
     id: "jdaffidas",
     imageSrc: "/assets/indexPage/desert_od_sljive_i_oraha.webp",
-    alt: "desert od šljive i oraha",
+    alt: "Desert od šljive i oraha",
     detailTitle: "Desert od šljive i oraha 50 grama",
     price: 89.0,
   },
   {
     id: "ddafnid00",
     imageSrc: "/assets/indexPage/ulje_od_kostica_sljive.webp",
-    alt: "ulje od koštica šljive",
+    alt: "Ulje od koštica šljive 250 ml",
     detailTitle: "Ulje od koštica šljive 250 ml, izvor vitamina E",
     price: 2000.0,
   },
   {
     id: "qddf4idas",
     imageSrc: "/assets/indexPage/ulje_od_kostica_sljive.webp",
-    alt: "ulje od koštica šljive",
+    alt: "Ulje od koštica šljive 100 ml",
     detailTitle: "Ulje od koštica šljive 100 ml, izvor vitamina E",
     price: 800.0,
   },
   {
     id: "caafgidas",
     imageSrc: "/assets/indexPage/orahovo_ulje.webp",
-    alt: "ulje od oraha",
+    alt: "Ulje od oraha 250 ml",
     detailTitle:
       "Ulje od oraha 250 ml, izvor vitamina i omega 3 masnih kiselina",
     price: 2000.0,
@@ -53,7 +53,7 @@ let shopItemsData = [
   {
     id: "fdaf6icas",
     imageSrc: "/assets/indexPage/orahovo_ulje.webp",
-    alt: "ulje od oraha",
+    alt: "Ulje od oraha 100 ml",
     detailTitle:
       "Ulje od oraha 100 ml, izvor vitamina i omega 3 masnih kiselina",
     price: 800.0,
